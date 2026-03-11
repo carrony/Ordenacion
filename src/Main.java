df2e3b1d59b8ee7995d91e8b8ae06f033b3d2643
adriancovenan-ops <adriancovenan@gmail.com>
2026-03-11 21:33:50 +0100

    Añadido el algoritmo de la burbuja Se ha añadido el algoritmo de la burbuja y un programa principal para probarlo.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    static void main(String[] args) {
+        int[] vector = {3,-7,8,4,9,2};
+        System.out.println("El vector tiene: " + Arrays.toString(vector));
+        Ordenaciones.burbuja(vector);
+        System.out.println("El vector ordenado es: " + Arrays.toString(vector));
+    }
+}
